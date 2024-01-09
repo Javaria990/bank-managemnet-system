dbdddddcd8577b0c3fdf4d6feb5fa7f7bdebf689
Javaria Ahmad <140826158+Javaria990@users.noreply.github.com>
2024-01-09 23:30:42 +0500

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,7 @@
+package OKAY;
+import java.io.IOException;
+public class Driver {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        new GUI();
+    }
+}
